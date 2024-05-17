ca58483e73b026e99dd21a2b8005a44a63f74f80
Dracowyn <dracowyn@outlook.com>
2024-05-17 11:42:07 +0800

    init

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<script setup>
+// Footer组件
+
+import {ref} from 'vue'
+// 当前年份
+const year = ref(new Date().getFullYear())
+</script>
+
+<template>
+    <div>
+        <h1>Footer</h1>
+        <p>版权所有 &copy; 2020 - {{ year }}</p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    h1 {
+        color: skyblue;
+    }
+    p {
+        color: #666;
+    }
+</style>
