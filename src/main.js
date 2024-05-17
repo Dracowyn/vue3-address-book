1cfda3b9012bcc3f8f812d5033f9e9580e774197
Dracowyn <dracowyn@outlook.com>
2024-05-17 11:51:42 +0800

    全量引入Vant

--- src/main.js
+++ src/main.js
@@ -4,8 +4,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import 'vant/lib/index.css'
+import Vant from 'vant'
+
 const app = createApp(App)
 
 app.use(router)
+
+app.use(Vant)
 
 app.mount('#app')
