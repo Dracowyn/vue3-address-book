405e79ab58d34b5d3d64a625ee1b91425a7ba94f
Dracowyn <dracowyn@outlook.com>
2024-05-20 09:18:07 +0800

    router以及导航栏的使用

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-
-    },
-  ]
+	// history模式，文档：https://next.router.vuejs.org/zh/guide/essentials/history-mode.html
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: '/',
+			name: 'Home',
+			component: () => import('../views/Home.vue')
+		},
+	]
 })
 
 export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Footer from "@/components/Footer.vue";
+
+</script>
+
+<template>
+	<van-nav-bar title="标题" />
+	<Footer/>
+</template>
+
+<style scoped lang="scss">
+
+</style>
