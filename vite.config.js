948a883938a51afcc1d0b089e532582e68ff93b3
Dracowyn <dracowyn@outlook.com>
2024-05-23 11:27:47 +0800

    修复第三方库找不到vue的问题

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+	plugins: [
+		vue(),
+	],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+			// 修复第三方库找不到vue的问题
+			vue: "vue/dist/vue.esm-bundler.js",
+		}
+	}
 })
