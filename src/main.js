0bf15579533c91cadb1d62d946a02bed12746492
Dracowyn <dracowyn@outlook.com>
2024-05-23 15:03:28 +0800

    引入Pinia改进

--- src/main.js
+++ src/main.js
@@ -8,16 +8,13 @@ import router from './router'
 import 'vant/lib/index.css'
 import Vant, {showNotify} from 'vant'
 
-
-import {useCookies} from "vue3-cookies";
-import business from "@/api/business.js";
+import {createPinia} from "pinia";
+import {useUserStore} from "@/store/user.js";
 
 const app = createApp(App)
-
 app.use(router)
-
+app.use(createPinia())
 app.use(Vant)
-
 app.mount('#app')
 
 // 全局路由守卫
@@ -28,47 +25,12 @@ app.router.beforeEach( async (to, from, next) => {
 	const auto = to.meta.auto === undefined ? true : to.meta.auto
 	// 如果需要登录
 	if (auto) {
-		// 获取cookie
-		const {cookies} = useCookies();
-		// 读取cookie
-		let userInfo = JSON.stringify(cookies.get('business'));
-		// 转换为对象
-		userInfo = JSON.parse(userInfo);
+		const userStore = useUserStore();
+		const userInfo = userStore.getUserInfo;
 
-		if (!userInfo) {
-			showNotify({
-				type: 'danger',
-				message: '请先登录',
-				duration: 1500,
-			});
+		if (!userInfo.id && !userStore.mobile) {
 			next('/login');
 			return;
-		}
-
-		// 获取ID和手机号
-		const id = userInfo.id;
-		const mobile = userInfo.mobile;
-
-		// 封装数据
-		const data = {
-			id: id,
-			mobile: mobile
-		}
-
-		// 发送请求
-		const result = await business.check(data);
-
-		console.log(result);
-
-		// 如果返回的code为0，说明登录失效
-		if (result.code === 0) {
-			showNotify({
-				type: 'danger',
-				message: result.msg,
-				duration: 1500,
-			});
-			next('/login');
-			return
 		}
 
 		// 如果登录有效，放行
